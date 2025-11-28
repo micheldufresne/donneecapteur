aa5f248f3ae5631deda4969ec3b6650974cd778e
Michel Dufresne <micdufresne@gmail.com>
2025-11-28 14:32:02 +0100

    mise en place de OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,58 @@
 #include "donneescapteur.h"
+#include "arduinoOTA.h"
+#define passwordOTA "tarte"
+const char *hostName = "essai";
+TabLastData<uint16_t> mesures(10, 0); // Tableau de 10 float, rempli avec 0
+const char *ssid = "piron";           // Enter SSID here
+const char *password = "Sivispacemparabellum60"; // Enter Password here
 
-Queue<float> mesures(5, 0.0f); // File de 5 float, remplie avec 0.0
+void initOTA()
+{
+  ArduinoOTA.setHostname(hostName);
+  ArduinoOTA.setPort(3232);
+  ArduinoOTA.setPassword(passwordOTA);
+}
+
+void demandeOTA()
+{
+  ArduinoOTA.begin();
+  ArduinoOTA.handle(); // test s'il y a une demande OTA
+}
+
+void testWifiOk()
+{
+  uint8_t nbEssais = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(2000);
+    nbEssais++;
+    if (nbEssais > 5)
+      ESP.restart();
+  }
+}
+
+void setupwifi()
+{
+  WiFi.begin(ssid, password);
+
+  // check wi-fi is connected to wi-fi network
+  testWifiOk();
+}
 
 void setup()
 {
   Serial.begin(115200);
 
-  mesures.remplir(2.0f);
-  mesures.push(3.0f);
-  mesures.push(4.0f);
-  mesures.push(5.0f);
-  mesures.push(6.0f);
+  //mesures.remplir(2u);
+  mesures.push(3);
+  mesures.push(4);
+  mesures.push(5);
+  mesures.push(6);
+  //mesures.remplir(2u);
+/*   mesures.push(3u);
+  mesures.push(4u);
+  mesures.push(5u);
+  mesures.push(6u); */
 
   Serial.print("Moyenne : ");
   Serial.println(mesures.moyenne(), 3);
@@ -20,6 +62,21 @@ void setup()
 
   Serial.print("Ecart type : ");
   Serial.println(mesures.ecartType(), 3);
+  setupwifi();
 }
 
-void loop() {}
+void loop() {
+
+  testWifiOk(); // vérifie le wifi et redémarre si ça tarde
+
+  demandeOTA();
+
+  uint16_t v=analogRead(34);
+  //Serial.println(v);
+  mesures.push(v);
+  float m = mesures.moyenne();
+  Serial.printf("v=%u  \tmoyenne=%.2f  \tecart=%.2f  \tecartType=%.2f\n",
+                              v, m, mesures.ecart(), mesures.ecartType());
+  
+  delay(500);
+}
